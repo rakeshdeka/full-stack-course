7f96d2d6c62bcd8654b40c65c6adbd057c297a3f
rakeshdeka <58238022+rakeshdeka@users.noreply.github.com>
2026-02-14 21:19:09 +0530

    class7

---
+++ class7/script.js
@@ -0,0 +1,146 @@
+// console.log("Hello, World! This is Class 7.");
+
+//variables
+// var, let, const
+
+// var a=2
+// var a=5
+
+// console.log(a)
+
+
+// let b=10
+//  b=20
+//  b=34
+
+// console.log(b)
+
+// const c=15;
+// c=34;
+// console.log(c);
+
+
+// datatypes in js
+// string, number, boolean, null, undefined, object
+
+// string
+
+// let name = "Sudeepta"
+// console.log(name);
+
+// number
+
+// let age = 25;
+// console.log(age);
+
+//boolean
+
+// let isStudent = true;
+// console.log(isStudent);
+
+//null
+
+// let city = null;
+// console.log(city);
+
+//undefined
+
+// let country;
+// console.log(country);
+
+// different between null and undefined(interview question)
+
+// object
+
+// let person = {
+//     name: "Sudeepta",
+//     age: 25,
+//     isStudent: true
+// }
+// console.log(person);
+
+
+// array
+// let students = ["siddharth", "navashmita", "sudeepta", 10, true, null, undefined]
+
+// console.log(students);
+
+
+// Operators in js
+
+// Arithmetic operators
+// +, -, *, /, %, ++, --
+// let a = 10;
+// let b = 5;
+// console.log(--a);
+
+
+
+//comparison operators
+// let x=10;
+// let y="20";
+// console.log(5 == "5");
+
+// number== string == true
+// console.log(5 === "5");
+
+
+// conditions
+
+// if , if else, else if
+
+
+// let age = 18;
+
+// if (age >= 18) {
+//     console.log("X is eligible to vote.");
+// }
+
+
+// if (age >= 18) {
+//     console.log("X is eligible to vote.");
+// } else {
+//     console.log("X is not eligible to vote.");
+// }
+
+// let marks = 50;
+
+// if (marks >= 90) {
+//     console.log("Grade A");
+// } else if (marks >= 75) {
+//     console.log("Grade B");
+// } else if (marks >= 40) {
+//     console.log("Grade C");
+// }else{
+//     console.log("Grade D");
+// }
+
+// ternary operator
+
+// let result = (age >= 18) ? "X is eligible to vote." : "X is not eligible to vote.";
+// console.log(result);
+
+
+// loops
+
+// for loops
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// i=5
+
+// 5<5 =false
+
+// 4++ = 5
+// break
+
+
+// while loop
+
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
