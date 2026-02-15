5e2526790f4055cf84f8a406481cb4cf50e18f44
rakeshdeka <58238022+rakeshdeka@users.noreply.github.com>
2026-02-15 19:44:44 +0530

    class8

---
+++ class8/script.js
@@ -0,0 +1,114 @@
+//Array
+
+// let fruits = ["apple", "banana", "orange", 1];
+
+// index starts from 
+
+// let fruits =[0,1,2]
+
+// console.log(fruits.length);
+
+// fruits.push("grape"); // add element at the end of the array
+// console.log(fruits);
+
+// fruits.pop(); // remove the last element from the array
+// console.log(fruits);
+
+// fruits.shift(); // remove the first element from the array
+// console.log(fruits);
+
+// fruits.unshift("mango"); // add element at the beginning of the array
+// console.log(fruits);
+
+// this are all methods of array
+
+// objects
+
+
+// let user = {
+//     name: "Rakesh",
+//     age: 25,
+//     isDeveloper: true,
+// }
+
+// console.log(user.name);
+// console.log(user.age);
+// user.city = "Guwahati";
+// user.name = "Rakesh Deka";
+// console.log(user);
+
+
+// function
+
+// function add() {
+//     console.log("Hello, Rakesh.");
+// }
+
+// add(); // function call
+
+// function with parameters
+
+// function add(a, b) {
+//     // console.log(a + b);
+//     return a + b;
+
+// }
+
+
+// console.log(add(5, 10));
+// console.log(add(10,20));
+// console.log(add(1,20));
+
+// Arrow function
+
+// const multiply = (a, b) => {
+//     return a * b;
+// }
+
+// console.log(multiply(4, 5));
+
+
+// function with default values
+
+// function greet(name="User"){
+//     console.log("Hello " + name);
+
+// }
+// greet("Mr. X");
+
+// DOM = Document Object Model
+
+// DOM Manupulation
+
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("click", function () {
+//     // alert("Button is clicked!");
+
+//     let title = document.getElementById("heading");
+//     title.innerText = "Button Clicked!";
+
+// })
+
+
+
+// function changeColor() {
+//     document.getElementById("box").style.backgroundColor = "red";
+// }
+
+
+
+
+// const btn = document.getElementById("toggleBtn");
+// btn.addEventListener("click", function () {
+//     document.body.classList.toggle("dark");
+// })
+
+
+
+
+
+
+
+
+
